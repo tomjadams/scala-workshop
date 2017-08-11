bcc2f4f43e3d190da27d5d3d3a59f7d8c147e00a
Tom Adams <tomjadams@gmail.com>
2017-08-11 11:37:39 +1000

    Additional tests.

--- example_code/src/test/scala/com/redbubble/pricer/BaseProductSpec.scala
+++ example_code/src/test/scala/com/redbubble/pricer/BaseProductSpec.scala
@@ -3,7 +3,15 @@ package com.redbubble.pricer
 import org.scalatest.{FlatSpec, Matchers}
 
 final class BaseProductSpec extends FlatSpec with Matchers {
-  "A base product" should "contain a product type " in {
+  "A base product" should "contain a product type" in {
     BaseProduct("hoodie", Map.empty, 0).productType shouldEqual "hoodie"
+  }
+
+  "A base product" should "contain options" in {
+    BaseProduct("hoodie", Map("k" -> Seq("v1", "v2")), 0).options shouldEqual Map("k" -> Seq("v1", "v2"))
+  }
+
+  "A base product" should "have a base price" in {
+    BaseProduct("hoodie", Map.empty, 10).basePrice shouldEqual 10
   }
 }
